4a9c10a87429d80aaf58c2c8824c53f5e0126215
Darthpool9900 <rafael.zachesky@gmail.com>
2022-08-17 10:54:09 -0300

    Terreno

--- TryCatch/Principal.java
+++ TryCatch/Principal.java
@@ -1,21 +1,46 @@
 import javax.swing.JOptionPane;
 import java.util.InputMismatchException;
-import java.util.Scanner;
+
 
 public class Principal {
     public static void main(String[] args) {
         String nome;
-        String nota,n_provas;
-        Scanner teste = new Scanner(System.in);
+        String nota,n_provas,ind;
         int nota_aux,num_provas,indice;
         
+        while(true){
+        ind = JOptionPane.showInputDialog(null,"Digite o indice que quer consultar:");
+        try{
+            indice=Integer.parseInt(ind);
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null,"Erro");
+        }
         nome = JOptionPane.showInputDialog(null,"Digite o nome:");
-        nota = JOptionPane.showInputDialog(null,"Nota:");
-        teste.nextInt();
-        n_provas = JOptionPane.showInputDialog(null,"Número de provas feitas:");
-        nota_aux = Integer.parseInt(nota);
-        num_provas = Integer.parseInt(n_provas);
+        try{
+           
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null,"Erro");
+            return;
+        }
+            nota = JOptionPane.showInputDialog(null,"Nota:");
+            try{
+                nota_aux= Integer.parseInt(nota);
+            }catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null,"Erro");
+                return;
+            }
+            n_provas = JOptionPane.showInputDialog(null,"Número de provas feitas:");
+            try{
+                num_provas = Integer.parseInt(n_provas);
+            }catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null,"Erro");
+                return;
+            }
+            try{
+                Aluno aluno[] = new Aluno[2];
+                aluno[indice]= new Aluno(nome,nota);
+            }
         
-
+    }
     }
 }
